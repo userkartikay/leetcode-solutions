4c281eb3c8447e630c83ff8c366d66d4e7752192
userkartikay <67700474+userkartikay@users.noreply.github.com>
2026-07-14 23:27:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.8%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        if(s=="") return true;
+        while(i<t.size()){
+            if(t[i]==s[j]){
+                i++;
+                j++;
+                if(j>= s.size()) return true;
+
+            }
+            else{
+            i++;
+            }
+
+        }
+        return false;
+
+    }
+};
